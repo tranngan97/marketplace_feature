2c824caa9a4735b8b61d28492721cdf1b0026209
tran ngan <ngan@betanet.co.il>
2023-10-16 17:06:06 +0700

    Seller Login Page, Seller Register Page

---
+++ app/code/MageStore/Marketplace/Controller/Seller/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MageStore\Marketplace\Controller\Seller;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Store\Model\ScopeInterface;
+
+class Index extends \Magento\Framework\App\Action\Action {
+
+    protected $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute() {
+        return $this->resultPageFactory->create();
+    }
+
+}
